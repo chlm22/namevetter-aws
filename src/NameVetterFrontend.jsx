83a4d7c4517ff52f1d8dd16bbe0812aeb8a2e242
Hae In Lee <haeinlee@umich.edu>
2026-08-01 19:31:09 -0400

    working on the frontend , namevetterfrontend,jsx

--- src/NameVetterFrontend.jsx
+++ src/NameVetterFrontend.jsx
@@ -12,13 +12,50 @@ export default function NameVetterFrontend(){
 
     const vetName = async(e) => {
         e.preventDefault();
+        //sanitizing and validating first before anything
 
         const cleanName = name.trim();
         //removes sneaky spaces at the beginning/end
         if(!cleanName){
-
+            setError("Please enter a name to vet.");
+            return;
         }//if(!cleanName)
 
-    } //const vetName
+        //too long of a word?
+        if(cleanName.length > 50){
+            setError("Name is too long. Keep it under 50 characters.");
+            return;
+        }//if(cleanName.length > 50)
+
+        //filter out the weird characters
+        //let the input only get the letters, spaces, hyphens and apostrophes
+        const nameRegex = /^[a-zA-Z\s'-]+$/;
+        if(!nameRegex.test(cleanName)){
+            setError("Invalid characters have been detected Please use only letters, spaces, hyphens and apostrophes.");
+            return;
+        }//nameRegex
+
+
+//this resets the stage by turning on the loading spinner/text 
+//clears previous results (or previous errors) before reaching out to AWS
+        setLoading(true);
+        setError(null);
+        setResult(null);
+        
+        //Sending a request to API Gatewat using the browser's built-in FETCH tool
+        try{
+            const response = await fetch(API_GATEWAY_URL, {
+                method: 'POST', // post request bc sending data
+                headers: {'Content-Type': 'application/json'}, // tells AWS that this is sending JSON data
+                body: JSON.stringify({targetName: name}),//body is taking the "name" the user typed and packaging it into an object and turns it into text string to let it travel thru the internet.
+            })//fetch
+            if (!response.ok){
+                throw new Error('HTTP error! status: ${response.status}');
+            }// if (!response.ok)
+        }//try
+        
+
+
+    } //const vetName = async(e)=>
 
 } //export default function NameVetterFrontend()
